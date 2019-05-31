83f6b94ffb0e07d177b96325ac1494a8b0ff3f1b
Morgan Kenyon <morganwkenyon@gmail.com>
2019-05-30 20:10:05 -0500

    Initial commit

---
+++ src/BoxingBenchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoxingBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
