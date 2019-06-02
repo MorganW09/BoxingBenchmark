57dc098b6203d7450d918f49bec2425d2e8c3e19
Morgan Kenyon <morganwkenyon@gmail.com>
2019-06-02 06:03:52 -0500

    Adding more boxing performance tests.

---
+++ src/BoxingBenchmark/BoxingStruct.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxingBenchmark
+{
+    public struct BoxingStruct
+    {
+        public int Id { get; set; }
+        public UserStruct User { get; set; }
+        public StatsStruct Stats { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is BoxingStruct))
+                return false;
+
+            BoxingStruct mys = (BoxingStruct)obj;
+            return mys.Id == Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+    }
+
+
+    public struct UserStruct
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public int age { get; set; }
+        public DateTime createdDate { get; set; }
+        public DateTime updateDate { get; set; }
+        public string password { get; set; }
+        public string profilePic { get; set; }
+    }
+
+    public struct StatsStruct
+    {
+        public int posts { get; set; }
+        public int comments { get; set; }
+        public int reads { get; set; }
+        public int likes { get; set; }
+        public string topPost { get; set; }
+        public string worstPost { get; set; }
+    }
+}
--- src/BoxingBenchmark/Program.cs
+++ src/BoxingBenchmark/Program.cs
@@ -5,39 +5,172 @@ using System.Collections.Generic;
 
 namespace BoxingBenchmark
 {
+    [MemoryDiagnoser]
     public class Runner
     {
         [Params(1000, 10_000)]
         public int N;
 
-        [Benchmark]
-        public void TestSimpleKey()
+        //[Benchmark(Baseline = true)]
+        //public void TestNormal()
+        //{
+        //    var dictionary = new Dictionary<int, int>();
+
+        //    for (int i = 0; i < N; i++)
+        //    {
+        //        if (!dictionary.ContainsKey(i))
+        //        {
+        //            dictionary.Add(i, i);
+        //        }
+        //    }
+        //}
+
+        //[Benchmark]
+        //public void TestBoxing()
+        //{
+        //    var dictionary = new Dictionary<object, int>();
+
+        //    for (int i = 0; i < N; i++)
+        //    {
+        //        if (!dictionary.ContainsKey(i))
+        //        {
+        //            dictionary.Add(i, i);
+        //        }
+        //    }
+        //}
+
+        //[Benchmark]
+        //public void TestBoxingAndInterface()
+        //{
+        //    IDictionary<object, int> dictionary = new Dictionary<object, int>();
+
+        //    for (int i = 0; i < N; i++)
+        //    {
+        //        if (!dictionary.ContainsKey(i))
+        //        {
+        //            dictionary.Add(i, i);
+        //        }
+        //    }
+        //}
+
+        [Benchmark(Baseline = true)]
+        public void TestStruct()
         {
-            var dictionary = new Dictionary<SimpleKey, int>();
+            var dictionary = new Dictionary<BoxingStruct, int>();
 
             for (int i = 0; i < N; i++)
             {
-                var newKey = new SimpleKey(i);
-                if (!dictionary.ContainsKey(newKey))
+                var boxingStruct = MakeBoxingStruct(i);
+                if (!dictionary.ContainsKey(boxingStruct))
                 {
-                    dictionary.Add(newKey, i);
+                    dictionary.Add(boxingStruct, i);
                 }
             }
         }
 
         [Benchmark]
-        public void TestPerformantKey()
+        public void TestStructObject()
         {
-            var dictionary = new Dictionary<PerformantKey, int>();
+            var dictionary = new Dictionary<object, int>();
 
             for (int i = 0; i < N; i++)
             {
-                var newKey = new PerformantKey(i);
-                if (!dictionary.ContainsKey(newKey))
+                var boxingStruct = MakeBoxingStruct(i);
+                if (!dictionary.ContainsKey(boxingStruct))
                 {
-                    dictionary.Add(newKey, i);
+                    dictionary.Add(boxingStruct, i);
                 }
             }
+        }
+
+        [Benchmark]
+        public void TestClass()
+        {
+            var dictionary = new Dictionary<object, int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                var boxingClass = MakeBoxingClass(i);
+                if (!dictionary.ContainsKey(boxingClass))
+                {
+                    dictionary.Add(boxingClass, i);
+                }
+            }
+        }
+
+        [Benchmark]
+        public void TestStructAndInterface()
+        {
+            IDictionary<object, int> dictionary = new Dictionary<object, int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                var boxingStruct = MakeBoxingStruct(i);
+                if (!dictionary.ContainsKey(boxingStruct))
+                {
+                    dictionary.Add(boxingStruct, i);
+                }
+            }
+        }
+
+        public BoxingStruct MakeBoxingStruct(int id)
+        {
+            var boxingStruct = new BoxingStruct()
+            {
+                Id = id,
+                User = new UserStruct()
+                {
+                    name = "Test User",
+                    email = "testemail@gmail.com",
+                    phone = "8293839283",
+                    age = 11110,
+                    createdDate = DateTime.UtcNow,
+                    updateDate = DateTime.UtcNow,
+                    password = "password1",
+                    profilePic = "profilePic.jpg"
+                },
+                Stats = new StatsStruct()
+                {
+                    posts = 10000,
+                    comments = 12000,
+                    reads = 100000,
+                    likes = -11000,
+                    topPost = "http://topPost",
+                    worstPost = "http://worstPost"
+                }
+            };
+
+            return boxingStruct;
+        }
+
+        public BoxingClass MakeBoxingClass(int id)
+        {
+            var boxingClass = new BoxingClass()
+            {
+                Id = id,
+                User = new UserClass()
+                {
+                    name = "Test User",
+                    email = "testemail@gmail.com",
+                    phone = "8293839283",
+                    age = 11110,
+                    createdDate = DateTime.UtcNow,
+                    updateDate = DateTime.UtcNow,
+                    password = "password1",
+                    profilePic = "profilePic.jpg"
+                },
+                Stats = new StatsClass()
+                {
+                    posts = 10000,
+                    comments = 12000,
+                    reads = 100000,
+                    likes = -11000,
+                    topPost = "http://topPost",
+                    worstPost = "http://worstPost"
+                }
+            };
+
+            return boxingClass;
         }
     }
 
